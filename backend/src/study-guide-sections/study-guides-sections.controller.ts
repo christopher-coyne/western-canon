a80980fe5890f2e45d0405f6115406f8e41aaaa3
christophercoyne <christcoyne@gmail.com>
2025-02-28 20:14:04 -0500

    feat: add section controllers + services

---
+++ backend/src/study-guide-sections/study-guides-sections.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { Result } from 'src/domain/result';
+import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
+import { StudyGuidesSectionsService } from './study-guides-sections.service';
+import { CreateStudyGuideQuestionDto } from './DTO/CreateStudyGuideQuestionDto';
+
+@Controller('/study-guides-sections')
+export class StudyGuidesController {
+  constructor(private readonly studyGuidesSectionsService: StudyGuidesSectionsService) {}
+
+  @Get('/:id/questions')
+  async getQuestions() {
+    return Result.ok(await this.studyGuidesSectionsService.getQuestions('id'));
+  }
+
+  @Post('/:id/question')
+  async createQuestion(@Req() req, @Param('id') id: string, @Body() body: CreateStudyGuideQuestionDto) {
+    const userId = req.user.id
+    return Result.ok(await this.studyGuidesSectionsService.createQuestion({data: body, sectionId: id, userId: id}));
+  }
+
+}
